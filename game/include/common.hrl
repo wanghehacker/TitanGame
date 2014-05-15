52496fcd2692f972ee52f86824ddf7ddef809636
wanghe <wanghewin7@163.com>
2014-05-15 23:06:27 +0800

    gateway

--- game/include/common.hrl
+++ game/include/common.hrl
@@ -33,3 +33,25 @@
 -define(ETS_STAT_SOCKET,ets_stat_socket).            %% Socket送出数据统计(协议号，次数)
 -define(ETS_STAT_DB,ets_stat_db).                    %% 数据库访问统计(表名，操作，次数)
 
+%%tcp_server监听参数
+-define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}, {nodelay, false}, {delay_send, true}, {send_timeout, 5000}, {keepalive, true}, {exit_on_close, true}]).
+-define(RECV_TIMEOUT, 5000).
+
+
+%% ---------------------------------
+%% Logging mechanism
+%% Print in standard output
+-define(PRINT(Format, Args),
+  io:format(Format, Args)).
+-define(TEST_MSG(Format, Args),
+  logger:test_msg(?MODULE,?LINE,Format, Args)).
+-define(DEBUG(Format, Args),
+  logger:debug_msg(?MODULE,?LINE,Format, Args)).
+-define(INFO_MSG(Format, Args),
+  logger:info_msg(?MODULE,?LINE,Format, Args)).
+-define(WARNING_MSG(Format, Args),
+  logger:warning_msg(?MODULE,?LINE,Format, Args)).
+-define(ERROR_MSG(Format, Args),
+  logger:error_msg(?MODULE,?LINE,Format, Args)).
+-define(CRITICAL_MSG(Format, Args),
+  logger:critical_msg(?MODULE,?LINE,Format, Args)).
--- game/src/ti_gateway_server.erl
+++ game/src/ti_gateway_server.erl
@@ -8,6 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(ti_gateway_server).
 -author("wanghe").
+-include("common.hrl").
 
 %% API
 -export([start_raw_server/3,
@@ -60,6 +61,76 @@ children(Port) when is_integer(Port)->
 port_name(Port) when is_integer(Port)->
     list_to_atom("ti_game"++integer_to_list(Port)).
 
-code_start(Master,Port,Fun,Max)
+%%打开监听
+%%Master：主进程
+%%Port:端口
+%%Fun:回调
+%%Max:最大连接数
+code_start(Master,Port,Fun,Max)->
+  process_flag(trap_exit,true),
+  case gen_tcp:listen(Port,?TCP_OPTIONS) of
+    {ok,Listen}->
+      Master!{self(),ok},
+      New = start_accept(Listen,Fun),
+      socket_loop(Listen,New,[],Fun,Max);
+    Error->
+      Master!{self(),Error}
+  end.
+
+%%循环接收外部连接
+%%Listen：
+%%New:当前外部连接子进程
+%%Active:所有的子进程
+%%Fun:回调
+%%Max:最大连接数
+socket_loop(Listen,New,Active,Fun,Max)->
+  receive
+    {istarted,New}->
+      Active1 = [New|Active],
+      possibly_start_another(false,Listen,Active1,Fun,Max);
+    {'EXIT',New,_Why}->
+      possibly_start_another(false,Listen,Active,Fun,Max);
+    {'EXIT',Pid,_Why}->
+      Active1 = lists:delete(Pid,Active),
+      possibly_start_another(New,Listen,Active1,Fun,Max);
+    {children,From}->
+      From!{session_server,Active},
+      socket_loop(Listen,New,Active,Fun,Max);
+    Other->
+      ?DEBUG("Here in loop:~p~n",[Other])
+  end.
+
+%%开始一个新的子进程
+possibly_start_another(New,Listen,Active,Fun,Max) when is_pid(New)->
+    socket_loop(Listen,New,Active,Fun,Max);
+
+possibly_start_another(false,Listen,Active,Fun,Max)->
+  case length(Active) of
+    N when N < Max ->
+      New = start_accept(Listen,Fun),
+      socket_loop(Listen,New,Active,Fun,Max);
+    _->
+      error_logger:warning_report(
+        [{module,?MODULE},
+          {line,?LINE},
+          {message,"connections maxed out"},
+          {maximum,Max},
+          {connected,length(Active)},
+          {now,now()}]),
+      socket_loop(Listen,false,Active,Fun,Max)
+  end.
+
+start_accept(Listen,Fun)->
+  S = self(),
+  spawn_link(fun() -> start_child(S,Listen,Fun) end).
+
+start_child(Parent,Listen,Fun)->
+  case gen_tcp:accept(Listen) of
+    {ok,Socket}->
+      Parent!{istarted,self()},
+      Fun(Socket);
+    _Other->
+      exit(oops)
+  end.
 
 
